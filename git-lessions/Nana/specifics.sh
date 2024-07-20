ea81a2d75be7e612f7f1c415c5517bf5018ae42f
princewillopah <princewillopah@gmail.com>
2024-07-20 18:19:10 +0000

    new push

---
+++ git-lessions/Nana/specifics.sh
@@ -0,0 +1,16 @@
+# ==================================================================================
+# Brashes
+# =================================================================
+cd path/to/your/repo 
+git checkout main               # Switch to the main branch:
+git pull origin main            # Pull the latest changes:
+git branch new-branch-name      #  Create a New Branch
+git checkout new-branch-name    #  Use the git checkout command to switch to the newly created branch:
+git push origin new-branch-name #If you want to push the new branch to the remote repository, use the git push command
+
+
+cd path/to/your/repo            # Navigate to the repository
+git checkout main               # Switch to the main branch:
+git pull origin main            # Pull the latest changes:
+git checkout -b feature-x       # Create and switch to a new branch:
+git push origin feature-x       # Push the new branch to the remote repository:
